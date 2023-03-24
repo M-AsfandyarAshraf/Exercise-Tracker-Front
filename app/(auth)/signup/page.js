0ceb71f6cc89d97b4fb70460e8764d66e530c2f5
M-AsfandyarAshraf <122967178+M-AsfandyarAshraf@users.noreply.github.com>
2023-03-24 17:46:00 +0500

    Fixed logo issue on signup page

--- app/(auth)/signup/page.js
+++ app/(auth)/signup/page.js
@@ -80,7 +80,7 @@ export default function Signup() {
   return (
     <>
     <div className=' h-screen flex flex-col gap-4 justify-center items-center ' style={inter.style}> 
-    <Image src={"/../public/Logo.png"} width = {100} height={100} alt={"Exercise Tracker Logo"}></Image>
+    <Image src={"/Logo.png"} width = {100} height={100} alt={"Exercise Tracker Logo"}></Image>
     <h1 className='text-7xl '>Sign-Up</h1>
     <span className='text-7lg '>Already a user?
       <Link href={'/login'}><span className='text-7lg text-blue-500'> Sign-In</span></Link>
